3864ab07ee1dab506e667cf2d30856bb653cbf2a
HuyTQ <huytq3103@gmail.com>
2021-09-26 22:22:22 +0700

    Add entity

---
+++ src/KnowledgeSpace.BackendServer/Data/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KnowledgeSpace.BackendServer.Data.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [MaxLength(50)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime Dob { get; set; }
+
+        public int? NumberOfKnowledgeBases { get; set; }
+
+        public int? NumberOfVotes { get; set; }
+
+        public int? NumberOfReports { get; set; }
+    }
+}
---
+++ src/KnowledgeSpace.BackendServer/Data/Interface/IDateTracking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KnowledgeSpace.BackendServer.Data.Interface
+{
+    public interface IDateTracking
+    {
+        DateTime CreateDate { get; set; }
+
+        DateTime? LastModifiedDate { get; set; }
+    }
+}
