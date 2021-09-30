4b8e94be83e41d6c546741f54eab868f7e1906ea
HuyTQ <huytq3103@gmail.com>
2021-09-30 23:36:36 +0700

    Command API

---
+++ src/KnowledgeSpace.BackendServer/Controllers/CommandsController.cs
@@ -0,0 +1,33 @@
+﻿using KnowledgeSpace.BackendServer.Data;
+using KnowledgeSpace.ViewModels.Systems;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommandsController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetCommants()
+        {
+            var commands = _context.Commands;
+
+            var commandVms = await commands.Select(u => new CommandVm()
+            {
+                Id = u.Id,
+                Name = u.Name,
+            }).ToListAsync();
+
+            return Ok(commandVms);
+        }
+    }
+}
---
+++ src/KnowledgeSpace.ViewModels/Systems/AddCommandToFunctionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace KnowledgeSpace.ViewModels.Systems
+{
+    public class AddCommandToFunctionRequest
+    {
+        public string CommandId { get; set; }
+
+        public string FunctionId { get; set; }
+    }
+}
