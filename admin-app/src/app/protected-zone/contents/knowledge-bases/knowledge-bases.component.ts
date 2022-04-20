31d0eeb517d58a909643b2583c8ff3440ca4a1d0
HuyTQ <huytq3103@gmail.com>
2022-04-20 14:23:31 +0700

    Structure Angular

---
+++ admin-app/src/app/protected-zone/contents/knowledge-bases/knowledge-bases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'knowledge-bases',
+  templateUrl: './knowledge-bases.component.html',
+  styleUrls: ['./knowledge-bases.component.css']
+})
+export class KnowledgeBasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
